a941ab42e1dde2ba5367b624912972fcd805dd96
anandkumarcoder <akanand104@gmail.com>
2021-08-26 00:01:56 +0530

    css done

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ const inputArea   = document.querySelector("#inputArea");
 const  outputArea = document.querySelector("#outputArea");
 const translateBtn  = document.querySelector("#translate");
 
+const serverApi = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(text){
+    return serverApi + "?" +"text=" + text
+};
+
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("something worng with the server try again later")
+}
+
 translateBtn.addEventListener("click",clickhandler);
 
 function clickhandler(){
-    console.log("test")
+    var inputText = inputArea.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+
+        outputArea.innerText = translatedText
+    })
+    .catch(errorHandler)
+    
 }
+
