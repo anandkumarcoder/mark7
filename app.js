c99601315f1f7bd37ebc8d11404adeaeaa8c2139
anandkumarcoder <akanand104@gmail.com>
2021-08-25 17:30:42 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const inputArea   = document.querySelector("#inputArea");
+const  outputArea = document.querySelector("#outputArea");
+const translateBtn  = document.querySelector("#translate");
+
+translateBtn.addEventListener("click",clickhandler);
+
+function clickhandler(){
+    console.log("test")
+}
